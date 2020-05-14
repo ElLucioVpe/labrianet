87bb702c2e66df71eb43dccb6936efc30fb01735
WhySoBizarre <pagolaguillen@hotmail.com>
2020-05-13 22:48:47 -0300

    ProyectoVisualSt

---
+++ Rest/NetRia/NetRia/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NetRia.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Rest/NetRia/NetRia/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetRia.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
