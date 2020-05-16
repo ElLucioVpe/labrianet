1f4959515f57007164c7289de45b9258887f25b9
WhySoBizarre <pagolaguillen@hotmail.com>
2020-05-16 00:06:10 -0300

    ClassesJE
    Weas de api

---
+++ Rest/NetRia/NetRia/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetRia.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult Add() {
+
+            using (Models.netriaEntities1 db= new Models.netriaEntities1()) {
+
+                var oUser = new Models.user();
+                oUser.nickUser = "Test";
+                oUser.passwordUser = "testJE";
+                oUser.loginnameUser = "Test";
+                oUser.fechaUser = DateTime.Now;
+                db.user.Add(oUser);
+                db.SaveChanges();
+
+            }
+
+            return Ok("New User Pimba");
+
+        }
+    }
+}
