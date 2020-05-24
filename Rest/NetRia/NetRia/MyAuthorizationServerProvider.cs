7a15d15b7665e5be6bcb150fb26ec69c986b52af
WhySoBizarre <pagolaguillen@hotmail.com>
2020-05-23 22:42:46 -0300

    AUTORIZANDO FOEEERTE
    auth System

---
+++ Rest/NetRia/NetRia/MyAuthorizationServerProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NetRia
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); // 
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            //Llamamos el login de estaban bro
+            //El UserName del context tendria que ser el correo si se pueden editar el nick, no me acuerdo xD LOQUENDO, mareado con el otro proyecto o que ase bordy
+            if (context.UserName == "user" && context.Password == "user")
+            {
+
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("correo", "correoUser"));
+                identity.AddClaim(new Claim("nick", "nickUser"));
+
+                context.Validated(identity);
+            }
+            else {
+                context.SetError("invalid_grant","Usuario o Password incorrecto");
+                return;
+            }
+        }
+    }
+}
