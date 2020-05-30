033f7216b004af8f46c694e53906134d15b313bd
Lucius <lucianoporta@outlook.com>
2020-05-29 23:15:52 -0300

    Arreglado login

--- App/webapp/src/Views/Login.js
+++ App/webapp/src/Views/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserContext from "../Libraries/ContextLib";
+import '../Css/Login.css';
 
 class Login extends React.Component {
     static contextType = UserContext
@@ -12,19 +13,19 @@ class Login extends React.Component {
 
     render() {
         return (
-          <div class="login">
-            <div>
-              <h1>Bienvenido!</h1>
-            </div>
-            <div>
-              <form>
-                <input class="input" type="text" name="name" placeholder="Usuario" />
-                <input class="input" type="password" name="password" placeholder="********" />
+            <div class="login">
+                <div>
+                    <h1>Bienvenido!</h1>
+                </div>
+                <div>
+                    <form>
+                        <input class="input" type="text" name="name" placeholder="Usuario"/>
+                        <input class="input" type="password" name="password" placeholder="********"/>
 
-                <input class="input" type="submit" class="btn-regular" value="Registrarme" />
-              </form>
+                        <input class="input" type="submit" class="btn-regular" value="Registrarme"/>
+                    </form>
+                </div>
             </div>
-          </div>
         );
     }
 }
