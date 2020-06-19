9080e2057f92ec80cc2ceab7815385b5c60116fd
Rigby <leandro.borges@utec.edu.uy>
2020-06-19 15:29:43 -0300

    Views en mobile y diagramas

--- App/webapp/src/index.js
+++ App/webapp/src/index.js
@@ -6,19 +6,20 @@ import Perfil from "./Views/Profile.js";
 import SignUp from "./Views/SignUp";
 import Login from "./Views/Login.js";
 import ConfigurarJuego from "./Views/ConfigurarJuego.js";
-import ComienzoJuego from "./Views/ComienzoJuego";
 import PlayerRanking from "./Views/PlayerRanking.js";
 import Header from "./Components/Header.js";
 import Loading from "./Components/Loading.js";
 import Grafica from "./Views/Grafica.js";
+import Enlace from "./Views/Enlace.js";
 import {UsuarioProvider, useUsuario} from "./Libraries/UserContextLib";
 import {JuegoProvider, useJuego} from "./Libraries/JuegoContextLib";
 import ThemeContext, {ThemeProvider} from "./Libraries/ThemeContextLib";
 import "./App.css";
-
+import Prejuego from "./Views/PreJuego.js"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Jugar from "./Views/Jugar";
 import Logout from "./Views/Logout";
+import PreJuego from "./Views/PreJuego";
 
 function QuizMaster() {
     return (
@@ -64,6 +65,9 @@ function App() {
                 <Route path="/crear">
                     <CrearJuego/>
                 </Route>
+                <Route path="/enlace">
+                    <Enlace/>
+                </Route>
                 <Route path="/jugar">
                     <Jugar/>
                 </Route>
@@ -79,6 +83,10 @@ function App() {
                 <Route path="/registro">
                     <SignUp/>
                 </Route>
+                <Route path="/prejuego">
+                    <PreJuego/>
+                </Route>
+
                 <Route path="/login">
                     <Login/>
                 </Route>
