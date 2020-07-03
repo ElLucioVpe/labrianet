988a636279dc3a0157e00ff35ef7c6eb8aad1b6b
Lucius <lucianoporta@outlook.com>
2020-07-02 21:46:59 -0300

    Profile no habia mergeado bien

--- App/webapp/src/Views/Profile.js
+++ App/webapp/src/Views/Profile.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import ProfileUserInfo from "../Components/ProfileUserInfo.js";
 import ProfileUserStats from "../Components/ProfileUserStats.js";
 import Button from "../Components/Button";
 import QuizMasterService from "../Libraries/QuizMasterServices";
-import { useUsuario } from "../Libraries/UserContextLib";
+import {useUsuario} from "../Libraries/UserContextLib";
 
 import "../Css/Profile.css";
 import QuizMasterServices from "../Libraries/QuizMasterServices";
 
 export default function Profile() {
-  const usuario = useUsuario();
-  const [juegos, setJuegos] = useState([]);
-  const [stats, setStats] = useState([]);
-  const [jugados, setJugados] = useState([]);
-  const [actualizar, setActualizar] = useState(false);
+    const usuario = useUsuario();
+    const [juegos, setJuegos] = useState([]);
+    const [stats, setStats] = useState([]);
+    const [jugados, setJugados] = useState([]);
+    const [actualizar, setActualizar] = useState(false);
 
     const desactivarJuego = (async (accion, pos) => {
         var game = juegos[pos];
-        if(accion === "Desactivar") game.activadoJuego = 0;
+        if (accion === "Desactivar") game.activadoJuego = 0;
         else game.activadoJuego = 1;
         //console.log(game);
         await QuizMasterService.updateJuego({
@@ -26,55 +26,54 @@ export default function Profile() {
         }).then(() => {
             setActualizar(!actualizar);
         });
-    });
-  };
+    };
 
-  useEffect(() => {
-    doIt();
+    useEffect(() => {
+        doIt();
 
-    async function doIt() {
-      if (usuario.usuario != null && usuario.accessToken != null) {
-        let data = await QuizMasterService.obtenerJuegosDeUsuario({
-          usuario: usuario.usuario,
-          accessToken: usuario.accessToken,
-        });
-        setJuegos(data);
+        async function doIt() {
+            if (usuario.usuario != null && usuario.accessToken != null) {
+                let data = await QuizMasterService.obtenerJuegosDeUsuario({
+                    usuario: usuario.usuario,
+                    accessToken: usuario.accessToken,
+                });
+                setJuegos(data);
 
-        let statsJuegosUsuario = await QuizMasterServices.obtenerJugadoresUsuario(
-          { loginname: usuario.usuario }
+                let statsJuegosUsuario = await QuizMasterServices.obtenerJugadoresUsuario(
+                    {loginname: usuario.usuario}
+                );
+                setStats(statsJuegosUsuario);
+
+                let data_jugados = await QuizMasterServices.obtenerArrayJugados({
+                    loginname: usuario.usuario,
+                });
+                setJugados(data_jugados);
+            }
+        }
+    }, [usuario.usuario, actualizar]);
+
+    function render() {
+        return (
+            <div class="profile">
+                <div class="usernameSection">
+                    <img className="profile-img" src="img/perfil.png"/>
+                    <h1>{usuario.usuario}</h1>
+                    <Button
+                        class="item"
+                        to="/crear"
+                        value="Crear quiz nueva"
+                        size="regular"
+                    />
+                </div>
+                <ProfileUserInfo juegos={juegos} stats={stats}/>
+                <ProfileUserStats
+                    juegos={juegos}
+                    jugados={jugados}
+                    desactivarJuego={desactivarJuego}
+                />
+            </div>
         );
-        setStats(statsJuegosUsuario);
-
-        let data_jugados = await QuizMasterServices.obtenerArrayJugados({
-          loginname: usuario.usuario,
-        });
-        setJugados(data_jugados);
-      }
     }
-  }, [usuario.usuario, actualizar]);
 
-  function render() {
-    return (
-      <div class="profile">
-        <div class="usernameSection">
-          <img className="profile-img" src="img/perfil.png" />
-          <h1>{usuario.usuario}</h1>
-          <Button
-            class="item"
-            to="/crear"
-            value="Crear quiz nueva"
-            size="regular"
-          />
-        </div>
-        <ProfileUserInfo juegos={juegos} stats={stats} />
-        <ProfileUserStats
-          juegos={juegos}
-          jugados={jugados}
-          desactivarJuego={desactivarJuego}
-        />
-      </div>
-    );
-  }
-
-  return render();
+    return render();
 }
